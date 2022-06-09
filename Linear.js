d3546d56b25346d5c90e0fe3f72a4bfdb99c23cd
Gurudutt Jangid <guruduttjangid@Gurudutts-MacBook-Air.local>
2022-06-09 15:04:18 +0530

    O(n) ---> Linear Time

--- Linear.js
+++ Linear.js
@@ -2,14 +2,11 @@ const nemo = ['Nemo'];
 const everyone = ['dory','bruce','marlin','nemo','gill','bloat','nigel','squirt','darla','hank'] // Everyone Array
 const large = new Array(1000000).fill('nemo');
 function findNemo(array){
-    let t0 = performance.now();
     for(i=0; i<array.length;  i++){
         if(array[i]=== 'nemo'){
             console.log('Found Nemo')
         }
     }
-    t1 = performance.now();
-    console.log('call to find nemo ' +(t1-t0)+ ' Miliseconds');
 }
 
-findNemo(large);
+findNemo(large); // O(n) ----> Linear Time
