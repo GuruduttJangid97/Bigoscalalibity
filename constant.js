1855b00b7e9ac7072dacd07bb7cfda319ed52caa
Gurudutt Jangid <guruduttjangid@Gurudutts-MacBook-Air.local>
2022-06-09 18:15:00 +0530

    O(1)----> Constant time

---
+++ constant.js
@@ -0,0 +1,9 @@
+// O(1) ----> Constant Time means no matter how many elements we have we selecting only one from the record
+// Notes: Keep in mind: Inputs can be any type of data, Not just array.
+const boxes = [1,2,3,4,5,6,7,8,9,10];
+
+function findingallBoxes(boxes){
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // 0(2)  
+}
+findingallBoxes(boxes); // O(2);
